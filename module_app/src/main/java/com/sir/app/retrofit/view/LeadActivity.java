b691925f49825bcadbab9f0479b9a2af57130d8d
zhuyinan <445181052@qq.com>
2017-06-13 15:38:54 +0800

    apk瘦身

--- module_app/src/main/java/com/sir/app/retrofit/view/LeadActivity.java
+++ module_app/src/main/java/com/sir/app/retrofit/view/LeadActivity.java
@@ -1,8 +1,14 @@
 package com.sir.app.retrofit.view;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
@@ -15,6 +21,8 @@ import com.hanks.htextview.HTextViewType;
 import com.sir.app.retrofit.R;
 import com.space.app.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import butterknife.Bind;
@@ -34,6 +42,8 @@ public class LeadActivity extends BaseActivity {
     @Bind(R.id.lead_title)
     HTextView title;
     HTextViewType[] type;
+    final int REQUEST_CODE_ASK_PERMISSIONS = 1001;
+
 
     @Override
     public int bindLayout() {
@@ -49,6 +59,8 @@ public class LeadActivity extends BaseActivity {
 
     @Override
     public void doBusiness(Context mContext) {
+
+
         animation = new ScaleAnimation(0.3f, 1f, 0.3f, 1f,
                 Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f);
@@ -86,10 +98,11 @@ public class LeadActivity extends BaseActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                getOperation().forward(MainActivity.class);
-                finish();
+                requestPermissions();
             }
         }, 3000);
+
+
     }
 
     private Handler handler = new Handler(Looper.getMainLooper());
@@ -99,5 +112,56 @@ public class LeadActivity extends BaseActivity {
         AlphaAnimation animation = new AlphaAnimation((float) 0.1, (float) 1.0);
         animation.setDuration(2000); //设置持续时间5秒
         imageView.startAnimation(animation);
+    }
+
+
+    /**
+     * 请求的权限
+     */
+    private void requestPermissions() {
+        //权限组
+        String[] permissionsArray = new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WAKE_LOCK,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS
+        };
+
+        //还需申请的权限列表
+        List<String> permissionsList = new ArrayList<>();
+
+        for (String permission : permissionsArray) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsList.add(permission);
+            }
+        }
+        if (!permissionsList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_ASK_PERMISSIONS);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                for (int i = 0; i < permissions.length; i++) {
+                    Log.e("TAG", permissions[i]);
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        //申请成功的权限
+                    } else {
+                        //权限被拒绝
+                    }
+                }
+                Log.e("TAG", "在请求权限的结果");
+                getOperation().forward(MainActivity.class);
+                finish();
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 }
